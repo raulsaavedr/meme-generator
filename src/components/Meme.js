1f0ef137fbeeaae99149c2edeb9d26cb7bf12793
Raul Saavedra <raulsdroid@gmail.com>
2022-03-23 21:29:25 -0400

    First Initial version

---
+++ src/components/Meme.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import "../css/Meme.css"
+
+export default function Meme() {
+    const [data, setData] = useState([]);
+    const [url, setUrl] = useState("https://i.imgflip.com/2d3al6.jpg");
+    
+    function handleGetImage(event) {
+        event.preventDefault();
+        setUrl(data.data.memes[Math.floor(Math.random() * 100)].url);
+        console.log(url);
+    }
+    function hoverImage(event) {
+        console.log(`Hovering the image, (x: ${event.pageX} y: ${event.pageY})`);
+    }
+
+    const getData = () => {
+        fetch('https://api.imgflip.com/get_memes')
+            .then(function (response) {
+                // console.log(response)
+                return response.json();
+            })
+            .then(function (myJson) {
+                console.log(myJson);
+                setData(myJson)
+            });
+    }
+    useEffect(() => {
+        getData()
+    }, [])
+
+    return (
+        <main>
+            <form action="">
+                <input type="text" placeholder="Top Text" />
+                <input type="text" placeholder="Bottom Text" />
+                <button onClick={handleGetImage}>Get a new meme image  🖼</button>
+            </form>
+            <img onMouseOver={hoverImage} src={url} alt="" />
+        </main>
+    );
+}
