67918df3ed7609ebe6917ced6a711e0a4b9b0fa5
Raul Saavedra <raulsdroid@gmail.com>
2022-03-25 21:31:08 -0400

    Add a carousel gallery to select base image meme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
-import "../css/Header.css"
-import trollIcon from "../images/troll-face.png"
+import "../css/Header.css";
+import trollIcon from "../images/troll-face.png";
 
 export default function Header() {
-    return (
-        <header>
-            <img src={trollIcon} alt="" />
-            <h3>Meme Generator</h3>
-            {/* <h5>React Course - Project 3</h5> */}
-        </header>
-    );
+  return (
+    <header>
+      <img src={trollIcon} alt="" />
+      <h3>Meme Generator</h3>
+      {/* <h5>React Course - Project 3</h5> */}
+    </header>
+  );
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,42 +1,42 @@
 import { useEffect, useState } from "react";
-import "../css/Meme.css"
+import "../css/Meme.css";
+import ImagesCarousel from "./Images";
 
 export default function Meme() {
-    const [data, setData] = useState([]);
-    const [url, setUrl] = useState("");
-    var seedrandom = require('seedrandom');
-    var prng = seedrandom();
-    function handleGetImage(event) {
-        event.preventDefault();
-        setUrl(data.data.memes[Math.floor(prng() * 100)].url);
-        // console.log(url);
-        // console.log(prng())
-    }
+  const [data, setData] = useState([]);
+  const [url, setUrl] = useState("");
+  var seedrandom = require("seedrandom");
+  var prng = seedrandom();
+  function handleGetImage(event) {
+    event.preventDefault();
+    setUrl(data.data.memes[Math.floor(prng() * data.data.memes.length)].url);
+  }
 
-    useEffect(() => {
-        const getData = () => {
-            fetch('https://api.imgflip.com/get_memes')
-                .then(function (response) {
-                    // console.log(response)
-                    return response.json();
-                })
-                .then(function (myJson) {
-                    console.log(myJson);
-                    setData(myJson)
-                    setUrl(myJson.data.memes[Math.floor(Math.random() * 100)].url);
-                });
-        };
-        getData();
-    }, []);
-    //
-    return (
-        <main>
-            <form action="">
-                <input type="text" placeholder="Top Text" />
-                <input type="text" placeholder="Bottom Text" />
-                <button onClick={handleGetImage}>Get a new meme image  🖼</button>
-            </form>
-            <img src={url} alt="" />
-        </main>
-    );
+  useEffect(() => {
+    const getData = () => {
+      fetch("https://api.imgflip.com/get_memes")
+        .then(function (response) {
+          // console.log(response)
+          return response.json();
+        })
+        .then(function (myJson) {
+          // console.log(myJson);
+          setData(myJson);
+          setUrl(myJson.data.memes[Math.floor(Math.random() * 100)].url);
+        });
+    };
+    getData();
+  }, []);
+  //
+  return (
+    <main>
+      <form action="">
+        <input type="text" placeholder="Top Text" />
+        <input type="text" placeholder="Bottom Text" />
+        <button onClick={handleGetImage}>Get a new random meme image 🖼</button>
+      </form>
+        {url && <ImagesCarousel memes={data.data.memes} setUrl={setUrl} />}
+      <img src={url} alt="" className="img-editing" />
+    </main>
+  );
 }
